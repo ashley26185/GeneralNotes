66fd9e440b1666a8225e5d3bac9dcaab05986379
Ashley L <ashley.a.lewis@gmail.com>
2024-06-07 19:00:29 +0400

    Update

--- Selenium/cookieclicker.py
+++ Selenium/cookieclicker.py
@@ -1,21 +1,50 @@
 from selenium import webdriver
-
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
-
-# https://sites.google.com/chromium.org/driver/getting-started
-# https://googlechromelabs.github.io/chrome-for-testing/#stable
-# https://storage.googleapis.com/chrome-for-testing-public/125.0.6422.141/mac-arm64/chromedriver-mac-arm64.zip
 
 service = Service(executable_path="./chromedriver")
 driver = webdriver.Chrome(service=service)
 
-driver.get("https://google.com")
+driver.get("https://orteil.dashnet.org/cookieclicker/")
 
-input_element = driver.get_element(By.CLASS_NAME, "fp-nh")
-time.sleep(20)
+# https://orteil.dashnet.org/cookieclicker/
 
-driver.quit()
+cookie_id = "bigCookie"
+cookies_id = "cookies"
+product_price_prefix = "productPrice"
+product_prefix = "product"
+
+WebDriverWait(driver, 5).until(
+    EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'English')]"))
+)
+
+language = driver.find_element(By.XPATH, "//*[contains(text(), 'English')]")
+language.click()
 
 
+WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.ID, cookie_id))
+)
+
+cookie = driver.find_element(By.ID, cookie_id)
+
+while True:
+    cookie.click()
+    cookies_count = driver.find_element(By.ID, cookies_id).text.split(" ")[0]
+    cookies_count = int(cookies_count.replace(",", ""))
+    
+    for i in range(4):
+        product_price = driver.find_element(By.ID, product_price_prefix + str(i)).text.replace(",", "")
+
+        if not product_price.isdigit():
+            continue
+
+        product_price = int(product_price)
+
+        if cookies_count >= product_price:
+            product = driver.find_element(By.ID, product_prefix + str(i))
+            product.click()
+            break
